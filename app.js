1a9cc23b7153704432b3e71117b808dabaee5cf3
java github <elaoufirn@gmail.com>
2024-08-14 23:56:32 +0100

    uppercase

--- app.js
+++ app.js
@@ -12,25 +12,46 @@
 //
 // })();
 
+// for the name calculator
+
+// (function (){
+//   'use strict';
+//
+//   angular.module('NameCalculator', [])
+//   .controller ('NameCalculatorController' , function($scope){
+//       $scope.name="";
+//       $scope.totalValue=0;
+//       $scope.displayNumeric = function (){
+//         let totalNameValue = calculateNum($scope.name);
+//         $scope.totalValue = totalNameValue;
+//
+//       function calculateNum( string){
+//           let totaStringValue = 0 ;
+//           for (let i = 0 ; i < string.length; i++){
+//             totaStringValue += string.charCodeAt(i);
+//           }
+//           return totaStringValue;
+//         }
+//       }
+//   });
+//
+// })();
+
 (function (){
+
   'use strict';
 
-  angular.module('NameCalculator', [])
-  .controller ('NameCalculatorController' , function($scope){
-      $scope.name="";
-      $scope.totalValue=0;
-      $scope.displayNumeric = function (){
-        let totalNameValue = calculateNum($scope.name);
-        $scope.totalValue = totalNameValue;
+  angular.module( "DiApp" , [])
+  .controller("DiController", function($scope , $filter){
 
-      function calculateNum( string){
-          let totaStringValue = 0 ;
-          for (let i = 0 ; i < string.length; i++){
-            totaStringValue += string.charCodeAt(i);
-          }
-          return totaStringValue;
-        }
-      }
-  });
+    $scope.name="nassim";
 
+    $scope.upper = function (){
+
+      let upCase = $filter('uppercase');
+      $scope.name= upCase($scope.name);
+
+    }
+
+  })
 })();
