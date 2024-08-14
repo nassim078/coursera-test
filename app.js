5319c2c1ae8a0624d0ea61119afb38785997178f
java github <elaoufirn@gmail.com>
2024-08-14 18:03:19 +0100

    name calculator

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-(function () {
-'use strict';
+// (function () {
+// 'use strict';
+//
+// angular.module('myFirstApp', [])
+//
+// .controller('MyFirstController', function ($scope) {
+//   $scope.name ="nassim"
+//   $scope.hello = function (){
+//     return "hello there";
+//   }
+// });
+//
+// })();
 
-angular.module('myFirstApp', [])
+(function (){
+  'use strict';
 
-.controller('MyFirstController', function () {
+  angular.module('NameCalculator', [])
+  .controller ('NameCalculatorController' , function($scope){
+      $scope.name="";
+      $scope.totalValue=0;
+      $scope.displayNumeric = function (){
+        let totalNameValue = calculateNum($scope.name);
+        $scope.totalValue = totalNameValue;
 
-});
+      function calculateNum( string){
+          let totaStringValue = 0 ;
+          for (let i = 0 ; i < string.length; i++){
+            totaStringValue += string.charCodeAt(i);
+          }
+          return totaStringValue;
+        }
+      }
+  });
 
 })();
